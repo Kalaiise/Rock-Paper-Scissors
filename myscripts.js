a367ddbd4230d02c98b66ca19015a59737b61b59
Jav <leducjavier14@gmail.com>
2023-09-24 11:52:54 -0400

    Updated old rps game to include basic UI

--- myscripts.js
+++ myscripts.js
@@ -1,61 +1,91 @@
 let playerScore = 0
 let compScore = 0
+let gameResult = document.querySelector('.gameResult');
 
-function getComputerChoice() {
+
+const score1 = document.querySelector('.score1');
+const score2 = document.querySelector('.score2');
+const p = document.createElement('p');
+const rockButton = document.querySelector('.rockButton');
+const paperButton = document.querySelector('.paperButton');
+const scissorButton = document.querySelector('.scissorButton');
+
+const computerChoice = () => {
 const choices = ["rock", "paper", "scissors"];
 const random = (Math.floor(Math.random() * choices.length));
 const randomChoices = choices[random];
     return randomChoices;
-}
+};
 
 
 
-function playRound(playerSelection, computerSelection) {
-    
+const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
-        return "Tie"
+        p.innerText = "Tie..."
+        gameResult.appendChild(p)
+
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore++
-        return "You win! Rock smashes scissors."
+        p.innerText = "You win! Rock smashes Scissors."
+        gameResult.appendChild(p)
         
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         compScore++
-        return "You lose! Paper covers rock."
+        p.innerText = "You lose! Paper covers Rock."
+        gameResult.appendChild(p) 
         
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore++
-        return "You win! Paper covers rock."
+        p.innerText = "You win! Paper covers Rock."
+        gameResult.appendChild(p) 
 
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         compScore++
-        return "You lose! Scissors cuts paper."
+        p.innerText = "You lose! Scissors cuts Paper."
+        gameResult.appendChild(p) 
         
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         compScore++
-        return "You lose! Rock smashes scissors."
+        p.innerText = "You lose! Rock smashes Scissors."
+        gameResult.appendChild(p) 
         
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore++
-        return "You win! Scissors cuts paper."
-        
-    };
-};
- 
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Lets play a game.. Choose Rock, Paper, or Scissors...").toLowerCase()
-        const computerSelection = getComputerChoice();
-        alert(playRound(playerSelection, computerSelection))
-    };
-     
-   if (playerScore > compScore) {
-        return "Nice! You wrecked the CPU.";
-    } else if (compScore > playerScore) {
-        return "You win some and you lose some...better luck next time...";
-    } else {
-        return "You tied with the CPU";
+        p.innerText = "You win! Scissors cuts Paper."
+        gameResult.appendChild(p) 
     };
 };
 
-alert(game());
+const updateScore = (playerScore, compScore) => {
+    score1.innerText = `Player Score: ${playerScore}`;
+    score2.innerText = `Computer Score: ${compScore}`;
+}; 
+
+const alertWinner = (playerScore, compScore) => {
+    if (playerScore === 5) {
+        alert("Nice! You wrecked the CPU.");
+    } else if (compScore === 5) {
+         alert("The robots have won...better luck next time...");
+    }
+};
+
+rockButton.addEventListener('click', () =>  {
+    const playerSelection = 'rock';
+    playRound(playerSelection, computerChoice())
+    updateScore(playerScore, compScore);
+    alertWinner(playerScore, compScore);
+});
+
+scissorButton.addEventListener('click', () =>  {
+    const playerSelection = 'scissors';
+    playRound(playerSelection, computerChoice())
+    updateScore(playerScore, compScore);
+    alertWinner(playerScore, compScore);     
+ });
+
+ paperButton.addEventListener('click', () =>  {
+    const playerSelection = 'paper';
+    playRound(playerSelection, computerChoice())
+    updateScore(playerScore, compScore);
+    alertWinner(playerScore, compScore);     
+});
